38218eb189bfc6bd2187c648631d7ff41ea33e8b
Carlos Eduardo Guimarães de Souza <eduardocarlos456@gmail.com>
2020-08-02 12:37:07 -0300

    Adicionado o mapeamento das entidades para o banco de dados

---
+++ src/DevIO.Data/Context/ECommerceCoreDbContext.cs
@@ -0,0 +1,37 @@
+﻿using DevIO.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DevIO.Data.Context
+{
+    public class ECommerceCoreDbContext : DbContext
+    {
+        public ECommerceCoreDbContext(DbContextOptions options) : base(options) { }
+
+
+        public DbSet<Produto> Produtos { get; set; }
+
+        public DbSet<Endereco> Enderecos { get; set; }
+
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var propriedade in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(x => x.GetProperties()
+                    .Where(y => y.ClrType == typeof(string))))
+                propriedade.Relational().ColumnType = "varchar(100)";
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ECommerceCoreDbContext).Assembly);
+
+            foreach (var relacionamento in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(x => x.GetForeignKeys()))
+                relacionamento.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
