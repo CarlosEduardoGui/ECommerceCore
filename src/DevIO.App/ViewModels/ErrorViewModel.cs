6d109e51e81a2348b84f71c09458e8e782e1cd30
Carlos Eduardo Guimarães de Souza <eduardocarlos456@gmail.com>
2020-08-08 23:37:26 -0300

    Adicionado validações de erros de páginas;

--- src/DevIO.App/ViewModels/ErrorViewModel.cs
+++ src/DevIO.App/ViewModels/ErrorViewModel.cs
@@ -1,11 +1,12 @@
-using System;
-
 namespace DevIO.App.ViewModels
 {
     public class ErrorViewModel
     {
-        public string RequestId { get; set; }
+        public int ErroCode { get; set; }
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public string Titulo { get; set; }
+
+        public string Mensagem { get; set; }
+
     }
 }
